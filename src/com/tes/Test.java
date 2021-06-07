3d3a36e37f7051598723bc01ab8c2b8a37ae729b
Nishanth6596 <nishanthimmanuel6596@gmail.com>
2021-06-07 20:48:56 +0530

    code newly added to nhg1

--- src/com/tes/Test.java
+++ src/com/tes/Test.java
@@ -4,5 +4,7 @@ public class Test {
 public static void main(String[] args) {
 	System.out.println("welcome");
 	System.out.println("hello");
+	System.out.println("coimbatore");
+	System.out.println("navalur");
 }
 }
